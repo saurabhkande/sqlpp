3fbf715d9b1d7a5b10ef0a7771dfc2d933ccc135
shaileshfc <60957758+shaileshfc@users.noreply.github.com>
2020-06-02 17:31:16 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,47 +1,20 @@
 USE TestDB -- you don't need the square brackets, but they don't hurt
 
-DROP TABLE Manager
-
-DROP TABLE Staff
-
--- Create ManagerTable
-CREATE TABLE Manager
-    (
-       id int IDENTITY(1,1),
-       name nvarchar(max),
-       CONSTRAINT pk_manager PRIMARY KEY (id)
-    )
-
-CREATE TABLE Staff
-    (
-       id int IDENTITY(1,1),
-       name nvarchar(max),
-       salary money,
-       managerId int FOREIGN KEY REFERENCES Manager(id) NULL,
-       CONSTRAINT pk_staff PRIMARY KEY (id)
-    )
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
 
 
---To populate Manager table:
-
-INSERT INTO [Manager]
+INSERT INTO Persons
     (
         -- id column value is auto-generated
-        name
+        LastName,FirstName
     )
     VALUES
     (
-       'John Doe'        
+       'John', 'Doe'        
     )    
-
---To populate Staff table:
-
-INSERT INTO [Staff]
-    (
-        -- id column value is auto-generated
-        name, salary, managerId
-    )
-    VALUES
-    (
-       'Jane Doe', 60000, 1       
-    )
