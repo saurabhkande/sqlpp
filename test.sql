bf095ab7bf44e8bd756db42f9f482dd8d449d525
shaileshfc <60957758+shaileshfc@users.noreply.github.com>
2020-06-02 13:27:03 +0530

    Create test.sql

---
+++ test.sql
@@ -0,0 +1,9 @@
+CREATE TABLE visits (
+    visit_id INT PRIMARY KEY IDENTITY (1, 1),
+    first_name VARCHAR (50) NOT NULL,
+    last_name VARCHAR (50) NOT NULL,
+    visited_at DATETIME,
+    phone VARCHAR(20),
+    store_id INT NOT NULL,
+    FOREIGN KEY (store_id) REFERENCES sales.stores (store_id)
+);
